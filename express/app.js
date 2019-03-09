7a2d414e0c95c562c2f423679c3d07dc4870edac
Hae Sang Ro <lemigeo@hotmail.com>
2019-03-09 15:57:18 +1100

    add apis

--- express/app.js
+++ express/app.js
@@ -5,6 +5,38 @@ const app = Express();
 app.use(Parser.json());
 app.use(Parser.urlencoded({ extended: true }));
 
+app.post('/user/create', async(req, res) => {
+    console.log(req.body.user_id);
+    const user = await Portal.createUser(req.body.user_id);
+    if(user && user.idx !== null){
+        res.status(200).send({
+            result: true,
+            idx: user.idx,
+            user_id : user.user_id,
+        });
+    }
+    else {
+        res.status(200).send({
+            result: false
+        });
+    }
+});
+
+app.get('/user/:idx', async(req, res) => {
+    const user = await Portal.getUser(req.params.idx);
+    if(user !== null) {
+        res.status(200).send({
+            result: true,
+            user: user
+        });
+    }
+    else {
+        res.status(200).send({
+            result: false
+        });
+    }
+});
+
 const server = app.listen(9091, function () {
     console.log('api server running');
 });
