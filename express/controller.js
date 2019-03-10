24da2021832ee5d9da6b512a37fd09b89173f613
Hae Sang Ro <lemigeo@hotmail.com>
2019-03-10 12:26:52 +1100

    add try catch

--- express/controller.js
+++ express/controller.js
@@ -1,18 +1,30 @@
 const Repository = require('./repository');
 
 module.exports.createUser = async(user_id) => {
-    let now = new Date();
-    let result = await Repository.createUser({
-        user_id: user_id,
-        create_dt: now,
-        update_dt: now
-    });
-    if(result !== null) {
-        return result.dataValues;
+    try {
+        let now = new Date();
+        let result = await Repository.createUser({
+            user_id: user_id,
+            create_dt: now,
+            update_dt: now
+        });
+        if(result !== null) {
+            return result.dataValues;
+        }
+        return result;
     }
-    return result;
+    catch(error) {
+        console.log(error);
+        return null;
+    }
 };
 
 module.exports.getUser = async(idx) => {
-    return await Repository.getUser(idx);
+    try {
+        return await Repository.getUser(idx);
+    }
+    catch(error) {
+        console.log(error);
+        return null;
+    }
 };
